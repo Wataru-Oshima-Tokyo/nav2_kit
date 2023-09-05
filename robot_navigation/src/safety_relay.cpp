add7175dba3321e18b96ca5c3573b10906b212a8
Wataru Oshima <wataru.bb.tokyo@gmail.com>
2023-09-05 21:30:23 +0900

    updated some values for amcl

--- robot_navigation/src/safety_relay.cpp
+++ robot_navigation/src/safety_relay.cpp
@@ -1,18 +1,25 @@
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/laser_scan.hpp"
 #include "geometry_msgs/msg/twist.hpp"
+#include "nav_msgs/msg/odometry.hpp"
 
-class WallFollower : public rclcpp::Node
+class SafetyRelay : public rclcpp::Node
 {
 public:
-    WallFollower()
-        : Node("wall_follower")
+    SafetyRelay()
+        : Node("saftey_relay")
     {
-        publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("limo/cmd_vel", 10);
+        cmd_vel_publisher_  = this->create_publisher<geometry_msgs::msg::Twist>("/diff_drive_base_controller/cmd_vel_unstamped", 10);
+        odom_sub_ = this->create_subscription<nav_msgs::msg::Odometry>(
+            "/diff_drive_base_controller/odom", 10,
+           std::bind(&SafetyRelay::odom_callback, this, std::placeholders::_1));
+        odom_pub_ = this->create_publisher<nav_msgs::msg::Odometry>("/odom", 10);
+
         scan_subscription_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
-            "/scan", rclcpp::SensorDataQoS(), std::bind(&WallFollower::scan_callback, this, std::placeholders::_1));
+            "/scan", rclcpp::SensorDataQoS(), std::bind(&SafetyRelay::scan_callback, this, std::placeholders::_1));
+        
         cmd_vel_subscription_ = this->create_subscription<geometry_msgs::msg::Twist>(
-            "/cmd_vel", rclcpp::SensorDataQoS(), std::bind(&WallFollower::cmd_vel_callback, this, std::placeholders::_1));
+            "/cmd_vel", rclcpp::SensorDataQoS(), std::bind(&SafetyRelay::cmd_vel_callback, this, std::placeholders::_1));
     }
 
 private:
@@ -53,27 +60,28 @@ private:
 
     void cmd_vel_callback(const geometry_msgs::msg::Twist::SharedPtr msg)
     {   
-        
-        if(fabs(msg->angular.z) < 0.5){
-            twist.angular.z = 0.5;
-            if (msg->angular.z < 0){
-                twist.angular.z  *= -1;
-            }
-            RCLCPP_WARN(this->get_logger(), "CHANGED CMD_VEL");
-        }else{
-            twist.angular.z = msg->angular.z; 
 
-        }
+        twist.angular.z = msg->angular.z;
         if(!warning || msg->linear.x < twist.linear.x)
             twist.linear.x = msg->linear.x; 
-        publisher_->publish(twist);
+        cmd_vel_publisher_->publish(twist);
     }
 
+    void odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg)
+    {
+        // Here you can manipulate the message if needed
+        // Currently, there is no manipulation happening.
 
+        // Publish the message to /odom
+        RCLCPP_WARN(this->get_logger(), "ODOM RECEIVED");
+        odom_pub_->publish(*msg);
+    }
 
-    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_publisher_;
     rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr scan_subscription_;
     rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_subscription_;
+    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
+    rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odom_pub_;
     geometry_msgs::msg::Twist twist;
     int angle_min_deg = -30;  // minimum angle in degrees
     int angle_max_deg = 30;   // maximum angle in degrees
@@ -81,6 +89,7 @@ private:
     // calculate the indices in the ranges list that correspond to the angles
     int index_min = (angle_min_deg - (-180)) / (angle_increment * 180 / M_PI);
     int index_max = (angle_max_deg - (-180)) / (angle_increment * 180 / M_PI);
+    
     bool warning = false;
     
 };
@@ -88,7 +97,7 @@ private:
 int main(int argc, char *argv[])
 {
     rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<WallFollower>());
+    rclcpp::spin(std::make_shared<SafetyRelay>());
     rclcpp::shutdown();
     return 0;
 }
