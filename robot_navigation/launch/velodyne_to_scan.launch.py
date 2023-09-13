0d388a71254fcbfe41ce96cd28dc02f6da6a74d7
Wataru Oshima <wataru.bb.tokyo@gmail.com>
2023-09-13 10:52:57 +0900

    udpated global costmap

--- robot_navigation/launch/velodyne_to_scan.launch.py
+++ robot_navigation/launch/velodyne_to_scan.launch.py
@@ -7,7 +7,11 @@ import os
 from ament_index_python.packages import get_package_share_directory
     
 def generate_launch_description():
+    theta_min_deg = -60
+    theta_max_deg = 60
 
+    theta_min_rad = theta_min_deg * math.pi / 180
+    theta_max_rad = theta_max_deg * math.pi / 180
     return LaunchDescription([
         Node(
             package='pointcloud_to_laserscan', executable='pointcloud_to_laserscan_node',
@@ -46,6 +50,25 @@ def generate_launch_description():
                 'inf_epsilon': 4.0
             }],
             name='pointcloud_to_laserscan_for_amcl'
+        ),
+        Node(
+            package='pointcloud_to_laserscan', executable='pointcloud_to_laserscan_node',
+            remappings=[('cloud_in', '/points_raw'),
+                        ('scan', '/scan_for_move')],
+            parameters=[{
+                'transform_tolerance': 0.01,
+                'min_height': 0.0,
+                'max_height': 2.0,
+                'angle_min': theta_min_rad,
+                'angle_max': theta_max_rad,
+                'angle_increment': 0.0087,  # M_PI/360.0
+                'scan_time': 0.3333,
+                'range_min': 0.1,
+                'range_max': 7.0,
+                'use_inf': False,
+                'inf_epsilon': 4.0
+            }],
+            name='pointcloud_to_laserscan_for_move'
         ),
 
     ])
