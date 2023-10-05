fcf771355c335cda813c020b07e33a8d821d0138
Wataru Oshima <wataru.bb.tokyo@gmail.com>
2023-10-05 14:18:37 +0900

    added pointcloud_trim

---
+++ pointcloud_trim/src/point_clouds_trim.cpp
@@ -0,0 +1,151 @@
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/point_cloud2.hpp>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/ModelCoefficients.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/filters/extract_indices.h>
+#include <nav2_msgs/srv/clear_entire_costmap.hpp>
+
+
+class PointCloudTrimmer : public rclcpp::Node
+{
+public:
+    PointCloudTrimmer() : Node("pointcloud_trimmer")
+    {
+            // Declare parameters with default values
+        this->declare_parameter<double>("min_height", 0.0);
+        this->declare_parameter<double>("max_height", 1.5);
+        this->declare_parameter<double>("angle_min", -60.0);
+        this->declare_parameter<double>("angle_max", 60.0);
+        this->declare_parameter<double>("range_min", 0.1);
+        this->declare_parameter<double>("range_max", 1.5);
+        this->declare_parameter<std::string>("point_clouds_topic", "trimmed_points");
+
+        // Get the parameters
+        this->get_parameter("min_height", min_height_);
+        this->get_parameter("max_height", max_height_);
+        this->get_parameter("angle_min", min_angle_);
+        this->get_parameter("angle_max", max_angle_);
+        this->get_parameter("range_min", min_length_);
+        this->get_parameter("range_max", max_length_);
+        this->get_parameter("point_clouds_topic", point_clouds_topic_);
+        publisher_ = this->create_publisher<sensor_msgs::msg::PointCloud2>(point_clouds_topic_, 10);
+        subscription_ = this->create_subscription<sensor_msgs::msg::PointCloud2>(
+            "points_raw", 10, std::bind(&PointCloudTrimmer::callback, this, std::placeholders::_1));
+                // Create the service client
+        clear_costmap_client_ = this->create_client<nav2_msgs::srv::ClearEntireCostmap>("global_costmap/clear_entirely_global_costmap");
+
+        // Setup the timer to call the service client every 2 seconds
+        timer_ = this->create_wall_timer(
+            std::chrono::seconds(2),
+            std::bind(&PointCloudTrimmer::clearCostmapCallback, this));
+    }
+
+private:
+
+        void clearCostmapCallback()
+        {
+            // Call the service to clear the global costmap
+            auto request = std::make_shared<nav2_msgs::srv::ClearEntireCostmap::Request>();
+            auto future = clear_costmap_client_->async_send_request(request);
+            // RCLCPP_INFO(this->get_logger(), "Clear cost map");
+
+        }
+
+        void callback(const sensor_msgs::msg::PointCloud2::SharedPtr msg)
+        {
+            pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>());
+            pcl::PointCloud<pcl::PointXYZ>::Ptr trimmed_cloud(new pcl::PointCloud<pcl::PointXYZ>());
+
+            pcl::fromROSMsg(*msg, *cloud);
+
+
+            for (const auto &point : cloud->points)
+            {
+                if (point.z >= min_height_ && point.z <= max_height_)
+                {
+                    double distance = std::hypot(point.x, point.y);
+                    double angle = std::atan2(point.y, point.x) * 180.0 / M_PI;
+
+                    if (angle >= min_angle_ && angle <= max_angle_ && distance >= min_length_ && distance <= max_length_)
+                    {
+                        trimmed_cloud->points.push_back(point);
+                    }
+                }
+            }
+
+
+            // Segment the ground
+            pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients());
+            pcl::PointIndices::Ptr inliers(new pcl::PointIndices());
+            pcl::SACSegmentation<pcl::PointXYZ> seg;
+            seg.setOptimizeCoefficients(true);
+            seg.setModelType(pcl::SACMODEL_PLANE);
+            seg.setMethodType(pcl::SAC_RANSAC);
+            seg.setDistanceThreshold(0.01);  // This threshold determines how close a point must be to the model in order to be considered an inlier.
+
+            seg.setInputCloud(trimmed_cloud);
+            seg.segment(*inliers, *coefficients);
+
+            if (inliers->indices.size() == 0)
+            {
+                RCLCPP_WARN(this->get_logger(), "Could not estimate a planar model for the given dataset.");
+                return;
+            }
+
+            // Extract non-ground points
+            pcl::ExtractIndices<pcl::PointXYZ> extract;
+            extract.setInputCloud(trimmed_cloud);
+            extract.setIndices(inliers);
+            extract.setNegative(true);
+            extract.filter(*trimmed_cloud);
+
+            // for (const auto &point : trimmed_cloud->points)
+            // {
+            //     if (point.z >= min_height_ && point.z <= max_height_)
+            //     {
+            //         double distance = std::hypot(point.x, point.y);
+            //         double angle = std::atan2(point.y, point.x) * 180.0 / M_PI;
+
+            //         if (angle >= min_angle_ && angle <= max_angle_ && distance >= min_length_ && distance <= max_length_)
+            //         {
+            //             trimmed_cloud->points.push_back(point);
+            //         }
+            //     }
+            // }
+
+            // trimmed_cloud->width = trimmed_cloud->points.size();
+            // trimmed_cloud->height = 1;
+
+            sensor_msgs::msg::PointCloud2 output;
+            pcl::toROSMsg(*trimmed_cloud, output);
+            output.header = msg->header;
+
+            publisher_->publish(output);
+        }
+
+
+    rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr publisher_;
+    rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr subscription_;
+    rclcpp::Client<nav2_msgs::srv::ClearEntireCostmap>::SharedPtr clear_costmap_client_;
+    rclcpp::TimerBase::SharedPtr timer_;
+
+    private:
+        double min_height_;
+        double max_height_;
+        double min_angle_;
+        double max_angle_;
+        double max_length_;
+        double min_length_;
+        std::string point_clouds_topic_;
+};
+
+int main(int argc, char **argv)
+{
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<PointCloudTrimmer>());
+    rclcpp::shutdown();
+    return 0;
+}
--- slope_detection/src/slope_detection.cpp
+++ slope_detection/src/slope_detection.cpp
@@ -8,6 +8,14 @@
 #include "tf2_ros/transform_listener.h"
 #include "tf2/convert.h"
 #include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
+// #include <pcl/conversions/from_pcl.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/common/centroid.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl_conversions/pcl_conversions.h>
 
 class SlopeDetectionNode : public rclcpp::Node
 {
@@ -16,90 +24,81 @@ public:
         : Node("slope_detection_node"), 
           tf_listener_(tf_buffer_, this)
     {
+        this->get_parameter("use_sim_time", use_sim_time_);
+        if (use_sim_time_)
+        {
+            this->set_parameter(rclcpp::Parameter("use_sim_time", true));
+        }
+
+
+
         lidar_subscriber_ = this->create_subscription<sensor_msgs::msg::PointCloud2>(
             "/points_raw", 10,
             std::bind(&SlopeDetectionNode::lidarCallback, this, std::placeholders::_1));
     }
 
 private:
-    void lidarCallback(const sensor_msgs::msg::PointCloud2::SharedPtr msg)
-    {
-        sensor_msgs::PointCloud2Iterator<float> iter_x(*msg, "x");
-        sensor_msgs::PointCloud2Iterator<float> iter_y(*msg, "y");
-        sensor_msgs::PointCloud2Iterator<float> iter_z(*msg, "z");
+        void lidarCallback(const sensor_msgs::msg::PointCloud2::SharedPtr msg)
+        {               
+            // Convert ROS PointCloud2 to PCL PointCloud
+            pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+            pcl::fromROSMsg(*msg, *cloud);
+            // Create the segmentation object
+            pcl::SACSegmentation<pcl::PointXYZ> seg;
+            pcl::PointIndices::Ptr inliers(new pcl::PointIndices);
+            pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients);
+            
+            seg.setOptimizeCoefficients(true);
+            seg.setModelType(pcl::SACMODEL_PLANE);
+            seg.setMethodType(pcl::SAC_RANSAC);
+            seg.setDistanceThreshold(0.01); // Adjust this threshold depending on the accuracy of your data
+
+            seg.setInputCloud(cloud);
+            seg.segment(*inliers, *coefficients);
+            if (inliers->indices.size() == 0)
+            {
+                RCLCPP_ERROR(this->get_logger(), "Could not estimate a planar model for the given dataset.");
+                slope_detected_ = false;
+                return;
+            }
+
+            // Here, coefficients->values have the coefficients of the detected plane ax+by+cz+d=0
+            // You can compute the normal to the plane and determine the slope
+
+            // Find centroid of the inliers (plane points)
+            Eigen::Vector4f centroid;
+            pcl::compute3DCentroid(*cloud, *inliers, centroid);
+            
+            // The centroid variable now has the x, y, z coordinates of the center of the plane
+            tf2::Vector3 detected_slope_position(centroid[0], centroid[1], centroid[2]);
+            
+            // Update slope_position_ and visualize in RViz
+            slope_position_ = detected_slope_position;
+            slope_detected_ = true;
+            publishSlopeTF();
+        }
+
+            void publishSlopeTF()
+            {
+                if (!slope_detected_)
+                    return;
+
+                // Create a transform for the detected slope in the "velodyne" frame
+                tf2::Transform tfSlopeInVelodyne;
+                tfSlopeInVelodyne.setIdentity();
+                tfSlopeInVelodyne.setOrigin(slope_position_);
+
+                geometry_msgs::msg::TransformStamped tfStamped;
+                tfStamped.header.stamp = this->get_clock()->now();
+                tfStamped.header.frame_id = "velodyne"; // parent frame
+                tfStamped.child_frame_id = "detected_slope"; // child frame
+                tfStamped.transform = tf2::toMsg(tfSlopeInVelodyne);
+
+                tf_broadcaster_->sendTransform(tfStamped);
+                RCLCPP_INFO(this->get_logger(), "Publish tfStamped");
+
+            }
         
-        float previous_z = *iter_z;
-        ++iter_z;
-
-        std::vector<tf2::Vector3> slope_points;
-
-        for (; iter_z != iter_z.end(); ++iter_z, ++iter_x, ++iter_y)
-        {
-            float current_z = *iter_z;
-            
-            float gradient = current_z - previous_z;
-            
-            if (gradient > threshold_)
-            {
-                tf2::Vector3 point(*iter_x, *iter_y, *iter_z);
-                slope_points.push_back(point);
-            }
-                
-            previous_z = current_z;
-        }
-
-        // Find the average position
-        tf2::Vector3 avg_point(0, 0, 0);
-        for (const auto& point : slope_points)
-        {
-            avg_point += point;
-        }
-        avg_point /= slope_points.size();
-
-        slope_detected_ = true;
-        slope_position_ = avg_point;
-        publishSlopeTF();
-    }
-
-        void publishSlopeTF()
-        {
-            if (!slope_detected_)
-                return;
-
-            geometry_msgs::msg::TransformStamped tfMapToVelodyne;
-            try 
-            {
-                tfMapToVelodyne = tf_buffer_.lookupTransform("map", "velodyne", rclcpp::Time(0));
-            }
-            catch (tf2::TransformException &ex) 
-            {
-                RCLCPP_WARN(this->get_logger(), "%s", ex.what());
-                return;
-            }
-
-            tf2::Transform tfMapToVelodyneTF;
-            tf2::fromMsg(tfMapToVelodyne.transform, tfMapToVelodyneTF);
-
-            // Convert slope position to a tf2::Transform
-            tf2::Transform tfSlopeInVelodyne;
-            tfSlopeInVelodyne.setIdentity();
-            tfSlopeInVelodyne.setOrigin(slope_position_);
-
-            tf2::Transform tfSlopeInMap = tfMapToVelodyneTF * tfSlopeInVelodyne;
-
-            geometry_msgs::msg::TransformStamped tfStamped;
-            tfStamped.header.stamp = this->get_clock()->now();
-            tfStamped.header.frame_id = "map";
-            tfStamped.child_frame_id = "detected_slope";
-
-
-            tfStamped.transform = tf2::toMsg(tfSlopeInMap);
-            tfStamped.transform.translation.x = 51.5;
-            tfStamped.transform.translation.y = 48.0;
-            tfStamped.transform.translation.z = 0.0;
-
-            tf_broadcaster_->sendTransform(tfStamped);
-        }
 
 
     rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr lidar_subscriber_;
@@ -108,6 +107,7 @@ private:
     tf2_ros::TransformListener tf_listener_;
     float threshold_ = 0.2;  
     bool slope_detected_ = false;
+    bool use_sim_time_;
     tf2::Vector3 slope_position_ = tf2::Vector3(0, 0, 0);
 
 };
