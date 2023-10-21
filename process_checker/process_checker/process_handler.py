e367a89ed285b4b6d4f5a59285e9056cbd738ac2
Wataru Oshima <wataru.bb.tokyo@gmail.com>
2023-10-21 21:30:15 +0900

    added msg sender service and fuction so that any node can send a messag to server thr rcs cclient

--- process_checker/process_checker/process_handler.py
+++ process_checker/process_checker/process_handler.py
@@ -7,6 +7,7 @@ from std_srvs.srv import Trigger
 import subprocess
 import time
 from .library.color_print import COLOR_PRINT
+from techshare_ros_pkg2.srv import SendMsg
 
 class NodeMonitor(Node):
     def __init__(self):
@@ -14,12 +15,15 @@ class NodeMonitor(Node):
         self.timer = self.create_timer(2.0, self.check_node_status)  # Check node status every 2 seconds
         self.killall_service = self.create_service(Trigger, 'killall', self.handle_killall)  # Create the killall service
         self.start_all_service = self.create_service(Trigger, 'restart', self.handle_startall)  # Create the restart service
+        self.rcs_send_msg_service = self.create_client(SendMsg, "send_msg")
         self.catmux_dict = os.getenv("WORK_SPACE")
         self.catmux_command = os.getenv("CATMUX_COMMAND")
         self.declare_parameter('docker', False)
+        self.declare_parameter('sim', False)
         self.declare_parameter('work_space', None)
         self.declare_parameter('catmux_command', "diffbot_in_ts_1st")
         self.docker = self.get_parameter("docker").get_parameter_value().bool_value 
+        self.sim = self.get_parameter("sim").get_parameter_value().bool_value 
         self.get_logger().info(f"docker {self.docker}")
         if self.catmux_dict is None:
             self.catmux_dict = self.get_parameter('work_space').value
@@ -28,7 +32,6 @@ class NodeMonitor(Node):
         self.color_print = COLOR_PRINT(self)
         self.color_print.print_in_blue(f"work_space {self.catmux_dict}")
         self.color_print.print_in_blue(f"catmux_command {self.catmux_command}")
-
         valid_dict = True
         if self.catmux_dict is None:
             self.color_print.print_in_red('Please specify the working space as WORK_SPACE') 
@@ -43,20 +46,37 @@ class NodeMonitor(Node):
         if os.path.exists(self.catmux_dict) and os.path.isdir(self.catmux_dict):
             self.color_print.print_in_green("YOU CAN HANDLE CATMUX COMMAND")
         self.started = False
+        self.now = time.time()
 
+    def isPassedTime(self, duration):
+        if (time.time() - self.now) > duration:
+            return True
+        else:
+            return False
 
 
             
     def discover_nodes(self):
         node_names_and_namespaces = self.get_node_names_and_namespaces()
-        self.get_logger().info('Discovered nodes: %s' % node_names_and_namespaces)
-        
-        if self.started and not self.is_all_nodes_ros_alive(node_names_and_namespaces):
+        # self.get_logger().info('Discovered nodes: %s' % node_names_and_namespaces)
+        if self.isPassedTime(3) and self.started and not self.is_all_nodes_ros_alive(node_names_and_namespaces):
+            req = SendMsg.Request()
+            req.message = "Failed to find witmotion node. Restarting soon"
+            req.error = True
+            self.rcs_send_msg_service.call_async(req)
+            time.sleep(1)
             self.startAction()  
-        
+        elif self.isPassedTime(3) and not self.isPassedTime(7) and self.started:
+            req = SendMsg.Request()
+            req.message = "Succeded to run all the nodes"
+            req.error = False
+            self.rcs_send_msg_service.call_async(req)
+
         return node_names_and_namespaces
 
     def is_all_nodes_ros_alive(self, nodes):
+        if self.sim:
+            return True
         for node_name, namespace in nodes:
             if not 'witmotion' in node_name and 'lio_sam_imuPreintegration' in node_name:  # Adjust this check as per the exact name of your node
                 return False
@@ -79,18 +99,24 @@ class NodeMonitor(Node):
 
     
     def startAll(self):
-            """
-            Open a new gnome-terminal, change to the specified directory, and run the command.
+        """
+        Open a new gnome-terminal, change to the specified directory, and run the command.
 
-            Args:
-            - directory (str): The directory to navigate to.
-            - session_name (str): The name for the catmux session.
-            """
-            # if self.docker:
-            #     command = f"gnome-terminal -- bash -c \"docker exec -it humble /bin/bash -c 'cd {self.catmux_dict} && catmux_create_session {self.catmux_command}.yaml;'\""
-            # else:
-            command = f'gnome-terminal -- bash -c "cd {self.catmux_dict} && catmux_create_session {self.catmux_command}.yaml;"'
-            subprocess.run(command, shell=True)
+        Args:
+        - directory (str): The directory to navigate to.
+        - session_name (str): The name for the catmux session.
+        """
+        # if self.docker:
+        #     command = f"gnome-terminal -- bash -c \"docker exec -it humble /bin/bash -c 'cd {self.catmux_dict} && catmux_create_session {self.catmux_command}.yaml;'\""
+        # else:
+        req = SendMsg.Request()
+        req.message = "Starting all the proesses"
+        req.error = True
+        self.rcs_send_msg_service.call_async(req)
+        command = f'gnome-terminal -- bash -c "cd {self.catmux_dict} && catmux_create_session {self.catmux_command}.yaml;"'
+        subprocess.run(command, shell=True)
+        self.started = True
+        self.now = time.time()
 
     def startAction(self):
         if self.started:
@@ -103,23 +129,27 @@ class NodeMonitor(Node):
             self.startAction()
             response.success = True
             self.get_logger().info("Start All the nodes")
-            self.started = True
         except subprocess.CalledProcessError:
             response.success = False
             response.message = "Failed to kill all ROS2 nodes."
         return response
 
     def killAll(self):
+        req = SendMsg.Request()
+        req.message = "Trying to kill all the process"
+        req.error = True
+        self.rcs_send_msg_service.call_async(req)
         try:
-            self.color_print.print_in_brown("First kill")
             cmd = "ps aux | grep ros |grep -v grep | grep -v process_handler |grep -v rcs_client_node | awk '{ print \"kill -9\", $2 }' | sh"
             subprocess.run(cmd, shell=True, check=True)
-            self.color_print.print_in_brown("Second kill")
             cmd = "ps aux | grep catmux | grep -v grep | grep -v process_handler | awk '{ print \"kill -9\", $2 }' | sh"
             time.sleep(2)
             subprocess.run(cmd, shell=True, check=True)
-            self.get_logger().info("All nodes except for 'process_handler' and 'rcs client' have been killed.")
+            self.color_print.print_in_brown("All nodes except for 'process_handler' and 'rcs client' have been killed.")
             self.started = False
+            req.message = "Killed all the processes"
+            req.error = False
+            self.rcs_send_msg_service.call_async(req)
         except subprocess.CalledProcessError:
             self.color_print.print_in_yellow("failed to execute it")
             
